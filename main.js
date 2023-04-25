17224a7395a49188a3a90b090da273e970b1f6f1
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-04-25 10:38:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function printHelloWorld(){
     console.log("Hello, World!");
 }
+
+/* Tabs */
+$(document).ready(function() {
+    $('.tab-links a').on('click', function(e) {
+        var currentAttrValue = $(this).attr('href');
+        $('.tab-content ' + currentAttrValue).fadeIn(400).siblings().hide();
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+        e.preventDefault();
+    });
+});
